c7accef839f1a8b88f705441e491581a42dc1995
9064674754 <98456102+9064674754@users.noreply.github.com>
2022-04-09 23:59:10 +0530

    Add files via upload

--- letschatroom.js
+++ letschatroom.js
@@ -27,10 +27,22 @@ var firebaseConfig = {
           });
     }
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-     Room_names = childKey;
-    //Start code
-
-    //End code
-    });});}
+    function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+      Room_names = childKey;
+     //Start code
+           console.log("room_name - " + Room_names);
+           row = "<div class='room_name' id="+Room_names+" onclick='redirectToroomname(this.id)'>"+Room_names+"</div><hr>";
+           document.getElementById("output").innerHTML += row;
+     //End code
+     });});}
 getData();
+function redirectToroomname(name){
+     console.log(name);
+     localStorage.setItem("Roomname",name);
+     window.location = "kwitter_page.html";
+}
+function logout() {
+     localStorage.removeItem("Username");
+     localStorage.removeItem("Roomname");
+     window.location = "index.html";
+}
